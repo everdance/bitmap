879605c995c10bbf8dc8d0676f093b47db344d8a
xiaolei <tristones.liu@gmail.com>
2024-08-30 21:55:41 -0400

    compare tuple values

--- bitmap.c
+++ bitmap.c
@@ -233,7 +233,7 @@ int _bm_find_dist_val_index(Datum *values, bool *isnull) {
     for (off = FirstOffsetNumber; off <= maxoff; off = OffsetNumberNext(off)) {
       ItemId		itid = PageGetItemId(page, off);
       IndexTuple	idxtuple = (IndexTuple) PageGetItem(page, itid);
-      if (is_vals_equal(values, isnull, idxtuple)) {
+      if (_bm_vals_equal(values, isnull, idxtuple)) {
         return index;
       }
 
@@ -359,4 +359,27 @@ static BitmapTuple *_bitmap_form_tuple(ItemPointer ctid) {
   tuple->bm[ctid->ip_posid/32] &= 0x1 << (ctid->ip_posid%32);
   
   return tuple;
+}
+
+bool _bm_vals_equal(Relation index, Datum *cmpVals, bools *cmpIsnull, IndexTuple itup) {
+  Datum		values[INDEX_MAX_KEYS];
+  bool		isnull[INDEX_MAX_KEYS];
+  TupleDesc tupDesc =  RelationGetDescr(index);
+  int nattrs = tupDesc->nattrs;
+
+  index_deform_tuple(itup, tupDesc, values, isnull);
+
+  for  (int i = 0; i < nattrs; i++) {
+    Form_pg_attribute att = TupleDescAttr(tupleDesc, i);
+
+    if (isnull[i] != cmpIsnull[i])
+      return false;
+
+    if (isnull[i]) continue;
+
+    if (!datumEqual(cmpVals[i], values[i], att->attbyval, att->attlen))
+      return false;
+  }
+
+  return true;
 }
