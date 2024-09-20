1a46e978b45f176cb74136a8d8deeafb99a12b8d
xiaolei <tristones.liu@gmail.com>
2024-09-20 11:48:22 -0400

    add test

--- bitmap--1.0.sql
+++ bitmap--1.0.sql
@@ -10,13 +10,92 @@ LANGUAGE C;
 CREATE ACCESS METHOD bitmap TYPE INDEX HANDLER bmhandler;
 COMMENT ON ACCESS METHOD bitmap IS 'bitmap index access method';
 
+-- Access operator classes
+CREATE OPERATOR CLASS int2_ops
+DEFAULT FOR TYPE int2 USING bitmap
+AS
+    OPERATOR        1       =,
+    FUNCTION        1       btint2cmp(int2,int2),
+STORAGE         int2;
+
 CREATE OPERATOR CLASS int4_ops
 DEFAULT FOR TYPE int4 USING bitmap
 AS
-    OPERATOR        1       =(int4, int4),
+    OPERATOR        1       =,
     FUNCTION        1       btint4cmp(int4,int4),
 STORAGE         int4;
 
+CREATE OPERATOR CLASS int8_ops
+DEFAULT FOR TYPE int8 USING bitmap
+AS
+    OPERATOR        1       =,
+    FUNCTION        1       btint8cmp(int8,int8),
+STORAGE         int8;
+
+CREATE OPERATOR CLASS float4_ops
+DEFAULT FOR TYPE float4 USING bitmap
+AS
+    OPERATOR        1       =,
+    FUNCTION        1       btfloat4cmp(float4,float4),
+STORAGE         float4;
+
+CREATE OPERATOR CLASS float8_ops
+DEFAULT FOR TYPE float8 USING bitmap
+AS
+    OPERATOR        1       =,
+    FUNCTION        1       btfloat8cmp(float8,float8),
+STORAGE         float8;
+
+CREATE OPERATOR CLASS timestamp_ops
+DEFAULT FOR TYPE timestamp USING bitmap
+AS
+    OPERATOR        1       =,
+    FUNCTION        1       timestamp_cmp(timestamp,timestamp),
+STORAGE         timestamp;
+
+CREATE OPERATOR CLASS timestamptz_ops
+DEFAULT FOR TYPE timestamptz USING bitmap
+AS
+    OPERATOR        1       =,
+    FUNCTION        1       timestamptz_cmp(timestamptz,timestamptz),
+STORAGE         timestamptz;
+
+CREATE OPERATOR CLASS inet_ops
+DEFAULT FOR TYPE inet USING bitmap
+AS
+    OPERATOR        1       =,
+    FUNCTION        1       network_cmp(inet,inet),
+STORAGE         inet;
+
+CREATE OPERATOR CLASS cidr_ops
+DEFAULT FOR TYPE cidr USING bitmap
+AS
+    OPERATOR        1       =(inet, inet),
+    FUNCTION        1       network_cmp(inet,inet),
+STORAGE         cidr;
+
+CREATE OPERATOR CLASS text_ops
+DEFAULT FOR TYPE text USING bitmap
+AS
+    OPERATOR        1       =,
+    FUNCTION        1       bttextcmp(text,text),
+STORAGE         text;
+
+CREATE OPERATOR CLASS varchar_ops
+DEFAULT FOR TYPE varchar USING bitmap
+AS
+    OPERATOR        1       =(text, text),
+    FUNCTION        1       bttextcmp(text,text),
+STORAGE         varchar;
+
+CREATE OPERATOR CLASS char_ops
+DEFAULT FOR TYPE "char" USING bitmap
+AS
+    OPERATOR        1       =,
+    FUNCTION        1       btcharcmp("char","char"),
+STORAGE         "char";
+
+-- Page inspection functions
 CREATE FUNCTION bm_metap(IN relname text,
     OUT magic text,
     OUT ndistinct int8,
@@ -24,7 +103,6 @@ CREATE FUNCTION bm_metap(IN relname text,
     OUT first_blks text)
 AS 'MODULE_PATHNAME', 'bm_metap'
 LANGUAGE C STRICT PARALLEL SAFE;
-
 
 CREATE FUNCTION bm_valuep(IN relname text, IN blkno int8,
     OUT index int4,
