3a49990e6fb3dab9ca61aa419e9533ff8f663b63
xiaolei <tristones.liu@gmail.com>
2024-09-22 21:38:30 -0400

    format

--- bmvacuum.c
+++ bmvacuum.c
@@ -13,104 +13,109 @@ bmbulkdelete(IndexVacuumInfo *info,
 			 IndexBulkDeleteCallback callback,
 			 void *callback_state)
 {
-  Relation index = info->index;
-  Buffer buffer;
-  BlockNumber blkno;
-  BitmapState state;
-  BitmapMetaPageData *meta;
-  Page page;
-  GenericXLogState *gxlogState;
-  BitmapPageOpaque opaque;
-  ItemPointer tids = palloc0(sizeof(ItemPointerData) * MAX_HEAP_TUPLE_PER_PAGE);
+	Relation	index = info->index;
+	Buffer		buffer;
+	BlockNumber blkno;
+	BitmapState state;
+	BitmapMetaPageData *meta;
+	Page		page;
+	GenericXLogState *gxlogState;
+	BitmapPageOpaque opaque;
+	ItemPointer tids = palloc0(sizeof(ItemPointerData) * MAX_HEAP_TUPLE_PER_PAGE);
 
-  if (stats == NULL)
-    stats = (IndexBulkDeleteResult *) palloc0(sizeof(IndexBulkDeleteResult));
+	if (stats == NULL)
+		stats = (IndexBulkDeleteResult *) palloc0(sizeof(IndexBulkDeleteResult));
 
-  buffer = ReadBuffer(index, BITMAP_METAPAGE_BLKNO);
-  LockBuffer(buffer, BUFFER_LOCK_SHARE);
+	buffer = ReadBuffer(index, BITMAP_METAPAGE_BLKNO);
+	LockBuffer(buffer, BUFFER_LOCK_SHARE);
 	meta = BitmapPageGetMeta(BufferGetPage(buffer));
 	blkno = meta->valBlkEnd;
 
-  state.ndistinct = meta->ndistinct;
-  if (state.ndistinct == 0)
-  {
-    UnlockReleaseBuffer(buffer);
-    return stats;
-  }
+	state.ndistinct = meta->ndistinct;
+	if (state.ndistinct == 0)
+	{
+		UnlockReleaseBuffer(buffer);
+		return stats;
+	}
 
-  state.blocks = palloc(sizeof(BlockNumber) * state.ndistinct);
-  memcpy(state.blocks, meta->firstBlk, sizeof(BlockNumber) * state.ndistinct);
+	state.blocks = palloc(sizeof(BlockNumber) * state.ndistinct);
+	memcpy(state.blocks, meta->firstBlk, sizeof(BlockNumber) * state.ndistinct);
 	UnlockReleaseBuffer(buffer);
 
-  for (int i = 0; i < state.ndistinct; i++)
-  {
-    blkno = state.blocks[i];
-    while (blkno != InvalidBlockNumber)
-    {
-      BitmapTuple *itup, *itupPtr, *itupEnd;
-      OffsetNumber maxoff;
+	for (int i = 0; i < state.ndistinct; i++)
+	{
+		blkno = state.blocks[i];
+		while (blkno != InvalidBlockNumber)
+		{
+			BitmapTuple *itup,
+					   *itupPtr,
+					   *itupEnd;
+			OffsetNumber maxoff;
 
-      vacuum_delay_point();
+			vacuum_delay_point();
 
-      buffer = ReadBuffer(index, blkno);
-      LockBuffer(buffer, BUFFER_LOCK_EXCLUSIVE);
-      gxlogState = GenericXLogStart(index);
-      page = GenericXLogRegisterBuffer(gxlogState, buffer, 0);
-      opaque = BitmapPageGetOpaque(page);
-      maxoff = opaque->maxoff;
+			buffer = ReadBuffer(index, blkno);
+			LockBuffer(buffer, BUFFER_LOCK_EXCLUSIVE);
+			gxlogState = GenericXLogStart(index);
+			page = GenericXLogRegisterBuffer(gxlogState, buffer, 0);
+			opaque = BitmapPageGetOpaque(page);
+			maxoff = opaque->maxoff;
 
-      itup = itupPtr = BitmapPageGetTuple(page, FirstOffsetNumber);
-      itupEnd = BitmapPageGetTuple(page, OffsetNumberNext(maxoff));
+			itup = itupPtr = BitmapPageGetTuple(page, FirstOffsetNumber);
+			itupEnd = BitmapPageGetTuple(page, OffsetNumberNext(maxoff));
 
-      while (itup < itupEnd)
-      {
-        int count = 0;
-        int delCount = 0;
-        
-        count = bm_tuple_to_tids(itup, tids);
-        for (int n = 0; n < count; n++) 
-        {
-          if (callback(&tids[n], callback_state))
-          {
-            int idx = tids[n].ip_posid - 1;
-            itup->bm[idx / 32] &= ~(0x1 << (idx % 32));
-            delCount++;
-            stats->tuples_removed += 1;
-          }
-        }
+			while (itup < itupEnd)
+			{
+				int			count = 0;
+				int			delCount = 0;
 
-        // all tuples deleted in a single index tuple, del index tuple
-        if (delCount == count)
-        {
-          opaque->maxoff--;
-        }
-        else
-        {
-          if (itupPtr != itup)
-            memmove((Pointer) itupPtr, (Pointer) itup, sizeof(BitmapTuple));
-          itupPtr++;
-        }
-        itup++;
-      }
+				count = bm_tuple_to_tids(itup, tids);
+				for (int n = 0; n < count; n++)
+				{
+					if (callback(&tids[n], callback_state))
+					{
+						int			idx = tids[n].ip_posid - 1;
 
-      Assert(itupPtr == BitmapPageGetTuple(page, opaque->maxoff));
+						itup->bm[idx / 32] &= ~(0x1 << (idx % 32));
+						delCount++;
+						stats->tuples_removed += 1;
+					}
+				}
 
-      if (itupPtr != itup)
-      {
-        if (opaque->maxoff == 0)
-        {
-            BitmapPageSetDeleted(page);
-        }
-        ((PageHeader) page)->pd_lower = (Pointer) itupPtr - page;
-        GenericXLogFinish(gxlogState);
-      } else {
-        GenericXLogAbort(gxlogState);
-      }
+				/* all tuples deleted in a single index tuple, del index tuple */
+				if (delCount == count)
+				{
+					opaque->maxoff--;
+				}
+				else
+				{
+					if (itupPtr != itup)
+						memmove((Pointer) itupPtr, (Pointer) itup, sizeof(BitmapTuple));
+					itupPtr++;
+				}
+				itup++;
+			}
 
-      blkno = opaque->nextBlk;
-      UnlockReleaseBuffer(buffer);
-    }
-  }
+			Assert(itupPtr == BitmapPageGetTuple(page, opaque->maxoff));
+
+			if (itupPtr != itup)
+			{
+				if (opaque->maxoff == 0)
+				{
+					BitmapPageSetDeleted(page);
+				}
+				((PageHeader) page)->pd_lower = (Pointer) itupPtr - page;
+				GenericXLogFinish(gxlogState);
+			}
+			else
+			{
+				GenericXLogAbort(gxlogState);
+			}
+
+			blkno = opaque->nextBlk;
+			UnlockReleaseBuffer(buffer);
+		}
+	}
 
 	return stats;
 }
@@ -119,14 +124,16 @@ bmbulkdelete(IndexVacuumInfo *info,
 IndexBulkDeleteResult *
 bmvacuumcleanup(IndexVacuumInfo *info, IndexBulkDeleteResult *stats)
 {
-  Relation	index = info->index;
-	BlockNumber preblk, blkno;
-  Buffer buffer, prevbuf;
-  int nvalues;
-  BlockNumber *blocks;
-  BitmapMetaPageData *meta;
-  Page page;
-  BitmapPageOpaque opaque;
+	Relation	index = info->index;
+	BlockNumber preblk,
+				blkno;
+	Buffer		buffer,
+				prevbuf;
+	int			nvalues;
+	BlockNumber *blocks;
+	BitmapMetaPageData *meta;
+	Page		page;
+	BitmapPageOpaque opaque;
 
 	if (info->analyze_only)
 		return stats;
@@ -134,57 +141,59 @@ bmvacuumcleanup(IndexVacuumInfo *info, IndexBulkDeleteResult *stats)
 	if (stats == NULL)
 		stats = (IndexBulkDeleteResult *) palloc0(sizeof(IndexBulkDeleteResult));
 
-  buffer = ReadBuffer(index, BITMAP_METAPAGE_BLKNO);
-  LockBuffer(buffer, BUFFER_LOCK_SHARE);
+	buffer = ReadBuffer(index, BITMAP_METAPAGE_BLKNO);
+	LockBuffer(buffer, BUFFER_LOCK_SHARE);
 	meta = BitmapPageGetMeta(BufferGetPage(buffer));
 	blkno = meta->valBlkEnd;
 
-  nvalues = meta->ndistinct;
-  if (nvalues == 0)
-  {
-    UnlockReleaseBuffer(buffer);
-    return stats;
-  }
+	nvalues = meta->ndistinct;
+	if (nvalues == 0)
+	{
+		UnlockReleaseBuffer(buffer);
+		return stats;
+	}
 
-  blocks = palloc(sizeof(BlockNumber) * nvalues);
-  memcpy(blocks, meta->firstBlk, sizeof(BlockNumber) * nvalues);
+	blocks = palloc(sizeof(BlockNumber) * nvalues);
+	memcpy(blocks, meta->firstBlk, sizeof(BlockNumber) * nvalues);
 	UnlockReleaseBuffer(buffer);
 
-  for (int i = 0; i < nvalues; i++)
-  {
-    blkno = blocks[i];
-    preblk = InvalidBlockNumber;
-    while (blkno != InvalidBlockNumber)
-    {
-      vacuum_delay_point();
+	for (int i = 0; i < nvalues; i++)
+	{
+		blkno = blocks[i];
+		preblk = InvalidBlockNumber;
+		while (blkno != InvalidBlockNumber)
+		{
+			vacuum_delay_point();
 
-      buffer = ReadBuffer(index, blkno);
-      LockBuffer(buffer, BUFFER_LOCK_EXCLUSIVE);
-      page = BufferGetPage(buffer);
-      opaque = BitmapPageGetOpaque(page);
-      
-      if (BitmapPageDeleted(page))
-      {
-         RecordFreeIndexPage(index, blkno);
-         stats->pages_free++;
-         if (preblk != InvalidBlockNumber)
-         {
-            prevbuf = ReadBuffer(index, preblk);
-            LockBuffer(prevbuf, BUFFER_LOCK_EXCLUSIVE);
-            BitmapPageGetOpaque(BufferGetPage(prevbuf))->nextBlk = opaque->nextBlk;
-            UnlockReleaseBuffer(prevbuf);
-         } else {
-          blocks[i] = opaque->nextBlk;
-         }
-      }
+			buffer = ReadBuffer(index, blkno);
+			LockBuffer(buffer, BUFFER_LOCK_EXCLUSIVE);
+			page = BufferGetPage(buffer);
+			opaque = BitmapPageGetOpaque(page);
 
-      preblk = blkno;
-      blkno = opaque->nextBlk;
-      UnlockReleaseBuffer(buffer);
-    }
-  }
-  
-  IndexFreeSpaceMapVacuum(info->index);
+			if (BitmapPageDeleted(page))
+			{
+				RecordFreeIndexPage(index, blkno);
+				stats->pages_free++;
+				if (preblk != InvalidBlockNumber)
+				{
+					prevbuf = ReadBuffer(index, preblk);
+					LockBuffer(prevbuf, BUFFER_LOCK_EXCLUSIVE);
+					BitmapPageGetOpaque(BufferGetPage(prevbuf))->nextBlk = opaque->nextBlk;
+					UnlockReleaseBuffer(prevbuf);
+				}
+				else
+				{
+					blocks[i] = opaque->nextBlk;
+				}
+			}
+
+			preblk = blkno;
+			blkno = opaque->nextBlk;
+			UnlockReleaseBuffer(buffer);
+		}
+	}
+
+	IndexFreeSpaceMapVacuum(info->index);
 
 	return stats;
 }
